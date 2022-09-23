1374961bee1f847c1e80f32d35f051b7dfa10106
takacsberni <takacsberni@gmail.com>
2022-09-23 12:48:13 +0200

    feat: create Profile.js and use it via App. It is for showing user information within a Json object

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import LoginButton from "./component/LoginButton";
 import LogoutButton from "./component/LogoutButton";
+import Profile from "./component/Profile";
 
 function App() {
   return (
       <div>
         <LoginButton />
         <LogoutButton />
+          <Profile />
       </div>
   );
 }
