949357ca0f00b0f5d985c99c293553353e05518e
takacsberni <takacsberni@gmail.com>
2022-09-23 13:03:15 +0200

    feat: show some information about the user, but only if the user is logged in

--- src/component/Profile.js
+++ src/component/Profile.js
@@ -3,13 +3,18 @@ import {useAuth0} from "@auth0/auth0-react";
 import JSONPretty from "react-json-pretty";
 
 const Profile = () => {
-    const {user} = useAuth0();
+    const {user, isAuthenticated} = useAuth0();
 
     return (
+        isAuthenticated && (
         <div>
+            <img src={user.profile} alt={user.name} />
+            <h2> {user.name} </h2>
+            <p> {user.email} </p>
             <JSONPretty data={user} />
             {/*{JSON.stringify(user, null, 2)};*/}
         </div>
+        )
     )
 };
 
