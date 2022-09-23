606fef5146d938ef66cf078c75e3956fbd511bc0
takacsberni <takacsberni@gmail.com>
2022-09-23 13:10:19 +0200

    feat: import and use isLoading functionality: if application is working in the background, display a text "Loadin...g"

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import './App.css';
 import LoginButton from "./component/LoginButton";
 import LogoutButton from "./component/LogoutButton";
 import Profile from "./component/Profile";
+import {useAuth0} from "@auth0/auth0-react";
 
 function App() {
+    const { isLoading } = useAuth0();
+
+    if (isLoading){
+        return <div> "Loading... " </div>
+    }
   return (
       <div>
         <LoginButton />
@@ -11,6 +17,7 @@ function App() {
           <Profile />
       </div>
   );
+
 }
 
 export default App;
